242fe4470a76a10cbcfe34c2d281e1c4515a14ee
Jahsh <joshua.espinas@gmail.com>
2017-09-17 12:52:08 +1200

    Created a Deck page

---
+++ project/Dominus.Web/Models/DeckModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace Dominus.Web.Models
+{
+  public class DeckModel
+  {
+    public string Name { get; set; }
+    public List<string> Cards { get; set; }
+  }
+}
